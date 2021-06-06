8cc58bdfe5c319907d77a140071808fb2c622415
mari4857 <mar4857@gmail.com>
2021-06-06 16:53:40 -0400

    setting up create method and list method for dishes router, and setting up list method for orders router

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,78 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+function list(request, response) {
+  response.json({ data: dishes });
+}
+
+function bodyHasName(request, response, next) {
+  const { data: { name } = {} } = request.body;
+  if (name) {
+    return next();
+  }
+  next({
+    status: 400,
+    message: "Dish must include a name",
+  });
+}
+
+function bodyHasDescription(request, response, next) {
+  const { data: { description } = {} } = request.body;
+  if (description) {
+    return next();
+  }
+  next({
+    status: 400,
+    message: "Dish must include a description",
+  });
+}
+
+function bodyHasPrice(request, response, next) {
+  const { data: { price } = {} } = request.body;
+  if (price && price > 0) {
+    return next();
+  }
+  next({
+    status: 400,
+    message: "Dish must include a price",
+  });
+}
+
+function bodyHasImgUrl(request, response, next) {
+  const { data: { image_url } = {} } = request.body;
+  if (image_url) {
+    return next();
+  }
+  next({
+    status: 400,
+    message: "Dish must include a image_url",
+  });
+}
+
+function create(request, response) {
+  const { data: { name } = {} } = request.body;
+  const { data: { description } = {} } = request.body;
+  const { data: { price } = {} } = request.body;
+  const { data: { image_url } = {} } = request.body;
+
+  const newDish = {
+    id: nextId(),
+    name,
+    description,
+    price,
+    image_url,
+  };
+  dishes.push(newDish);
+  response.status(201).json({ data: newDish });
+}
+
+module.exports = {
+  list,
+  create: [
+    bodyHasName,
+    bodyHasDescription,
+    bodyHasPrice,
+    bodyHasImgUrl,
+    create,
+  ],
+};
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,5 +1,12 @@
-const router = require("express").Router();
+const router = require("express").Router({ mergeParams: true });
+const controller = require("./dishes.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
 // TODO: Implement the /dishes routes needed to make the tests pass
+router
+  .route("/")
+  .get(controller.list) //list
+  .post(controller.create) //create
+  .all(methodNotAllowed); //validation
 
 module.exports = router;
